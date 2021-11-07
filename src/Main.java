56be99c5c220ac49de9f8f8a99a023117f81a77f
Aomine2K <75029082+Aomine2K@users.noreply.github.com>
2021-11-07 17:34:34 +0100

    zad.3.final

--- src/Main.java
+++ src/Main.java
@@ -54,7 +54,7 @@ public class Main {
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
-            */
+
 
         String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
         int[] tablica = {0, 0, 0, 0, 0, 0};
@@ -79,11 +79,36 @@ public class Main {
         }
         System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy - razy\nz: %d - razy\nq: %d ",
                 tablica[0], tablica[1], tablica[2],tablica[3],tablica[4],tablica[5] );
-
+*/
           /*
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
-
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] tablica = {0, 0, 0, 0, 0, 0,0};
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'o':
+                    ++tablica[0];
+                    break;
+                case 'p':
+                    ++tablica[1];
+                    break;
+                case 'x':
+                    ++tablica[2];
+                    break;
+                case 'z':
+                    ++tablica[3];
+                    break;
+                case 'q':
+                    ++tablica[4];
+                    break;
+                case 'j','i','t','y','n','m':
+                    ++tablica[5];
+                    break;
+            }
+        }
+        System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy - razy\nz: %d - razy\nq: %d - razy\nPozostale: %d ",
+                tablica[0], tablica[1], tablica[2],tablica[3],tablica[4],tablica[5],tablica[6] );
 
     }
 }
